46335092d0935257004175df9e80345dc963321d
Jeremiah Jekich <jjekich@gmail.com>
2014-05-20 11:46:35 -0600

    Added main visualize method to the visualizer.
    Handles main logic for the creation and drawing of visualization
    objects.

--- Sprint1/script/visualizer.js
+++ Sprint1/script/visualizer.js
@@ -6,3 +6,24 @@ function Line(dataSet) {
 Line.prototype.draw = function () {
 	//d3.js code here
 }
+
+function visualize(dataPackage, parentId) {
+	var visualizations = extractVisualizations(dataPackage);
+
+	var divId = "vis";
+
+	var numVisualizations = visualizations.length;
+	for (var i = 0; i < numVisualizations; i++) {
+		divId = divId + "i";
+		createDiv(parentId, divId);
+		visualizations[i].draw(divId);
+	}
+}
+
+function extractVisualizations(dataPackage) {
+	
+}
+
+function createDiv(parentId, divId) {
+
+}
