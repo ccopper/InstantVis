65bca3aa16c56576dda2a49fc72503a00d3d4bc4
ccopper <ccopper@mymail.mines.edu>
2014-05-21 05:54:00 -0600

    Updated API

--- Sprint1/script/InstantVisAPI/InstantVisAPI.js
+++ Sprint1/script/InstantVisAPI/InstantVisAPI.js
@@ -1,16 +1,53 @@
-/*	Instant Vis API
+/**
+ *	Instant Vis API
  *
- *	Requires jQuery 2.1
+ *	@module InstantVisAPI
  */
 
+ /**
+  *	JSON For the default API Configuration. 
+  *
+  * @property InstantVisAPI_Config
+  * @type JSON
+  */
 var InstantVisAPI_Config = 
 { 
-	"Host": 
+	"Host": "http://localhost:3000/"
 }
 
-
-
-function parseHTML(url)
+/**
+ *	Requests that the provided URL be scraped for tables.
+ *
+ *	@method parseHTML
+ *	@param {string} URL				The URL to Scrape
+ *	@param {Function} APICallBack	The Resulting callback when the request completes
+ *	
+ */
+function parseHTML(URL, APICallback)
 {
-	
+	$.ajax({
+		type: 'GET',
+        url: InstantVisAPI_Config.Host + "parseHTML/" + URL,
+        dataType: "json", 
+		success: function(data, textStatus, jqXHR)
+		{
+			APICallback(data);
+		},
+		error: InstantVisAPI_Error
+		
+	});	
 }
+
+/**
+ *	Requests that the provided URL be scraped for tables.
+ *
+ *	@method InstantVisAPI_Error
+ *	@param {string} URL				The URL to Scrape
+ *	@param {Function} APICallBack	The Resulting callback when the request completes
+ *	
+ */
+function InstantVisAPI_Error(jqXHR, textStatus, errorThrown)
+{
+	alert("API Call Failed: " + errorThrown); 
+}
+
