40b647c9772e5d0436bf97b3e34ce4027d948b55
eittreim <eittreim@server.fake>
2014-06-11 12:27:35 -0600

    AI: had been looking for DataColumns in the wrong place.

--- Sprint3/script/AI.js
+++ Sprint3/script/AI.js
@@ -332,18 +332,18 @@ var generateVisTitle = function(AIdataStructure)
 
 			if (visualization.DataColumns.length == 2) // one independent and one dependent 
 			{
-				visTitle = "" + element.ColumnLabel[visualization.DataColumns[1]] + " vs " + 
-					element.ColumnLabel[visualization.DataColumns[0]];
+				visTitle = "" + element.Data.ColumnLabel[visualization.DataColumns[1]] + " vs " + 
+					element.Data.ColumnLabel[visualization.DataColumns[0]];
 			}
 			else if (visualization.DataColumns.length == 3) // one independent and two dependents
 			{
-				visTitle = "" +  element.ColumnLabel[visualization.DataColumns[1]] + " and " + 
-				  	element.ColumnLabel[visualization.DataColumns[2]] + " vs " + 
-					element.ColumnLabel[visualization.DataColumns[0]];
+				visTitle = "" +  element.Data.ColumnLabel[visualization.DataColumns[1]] + " and " + 
+				  	element.Data.ColumnLabel[visualization.DataColumns[2]] + " vs " + 
+					element.Data.ColumnLabel[visualization.DataColumns[0]];
 			}
 			else // some other combo of vars, just use the independent var name as the title
 			{
-				visTitle = "" + element.ColumnLabel[visualization.DataColumns[1]];
+				visTitle = "" + element.Data.ColumnLabel[visualization.DataColumns[1]];
 			}
 			
 			visualization.VisTitle = visTitle;
